c95436465942351f001103ffd09d1edde14fa4fa
unknown <ifollowyou@aliyun.com>
2015-12-14 18:59:47 +0800

    update tomcat official examples

---
+++ websocket-tomcat-examples/src/main/java/websocket/drawboard/DrawboardContextListener.java
@@ -0,0 +1,21 @@
+package websocket.drawboard;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public final class DrawboardContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // NO-OP
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Shutdown our room.
+        Room room = DrawboardEndpoint.getRoom(false);
+        if (room != null) {
+            room.shutdown();
+        }
+    }
+}
---
+++ websocket-tomcat-examples/src/main/webapp/websocket/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <title>Apache Tomcat WebSocket Examples</title>
+</head>
+<body>
+<h1>Apache Tomcat WebSocket Examples</h1>
+<ul>
+    <li><a href="echo.jsp">Echo example</a></li>
+    <li><a href="chat.jsp">Chat example</a></li>
+    <li><a href="snake.jsp">Multiplayer snake example</a></li>
+    <li><a href="drawboard.jsp">Multiplayer drawboard example</a></li>
+
+</ul>
+</body>
+</html>
